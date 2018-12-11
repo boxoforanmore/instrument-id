6fdc6aeb082a530371aed9c0a2427d7f815da7f9
Peter Nielson <computergeekpete@gmail.com>
2018-12-11 15:50:39 -0700

    Fixed output in file trimmer script

--- trimFiles.sh
+++ trimFiles.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+cd data/instruments/
+
+echo "Collecting 30 second snippets from various tunes"
+
+declare -i total=0
+
 for fname in *
 do
 
   echo "Looking at $fname"
-  if [-d "$fname"]
+  if [ -d "$fname" ]
   then
 
-    echo "Trimming files in $fname to be from 30 seconds long (20s-50s)..."
+    echo "Trimming files in $fname to be 30 seconds long (20s-50s)..."
     cd "$fname"
-    number = 0
+    declare -i number=0
 
     for f in *.mp3
     do
-      new_f = "$fname$number"
-      sox "$f" "$fname$number.mp3" trim 20 50
+      echo "Trimming file number: $number..."
+      echo "Filename: $f"
+
+      sox "$f" "$fname$number.mp3" trim 20 30
       (( ++number ))
+      (( ++total ))
     done
+    cd ..
+
+    echo
+    echo 
+    echo "Trimmed $number items from $fname"
+    echo
+    echo "Total trimmed so far => $total"
 
   fi
-
 done
+
+cd ../../
