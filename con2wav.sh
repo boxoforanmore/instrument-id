ff4fe9f038b5e54803ef6a6d3a0f29661b9fb3d6
Peter Nielson <computergeekpete@gmail.com>
2018-12-10 17:50:38 -0700

    Added mp3 to wav converter

---
+++ con2wav.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo
+echo "Converting tune_type data from mp3 to more wav for preprocessing..."
+echo
+
+cd data/instruments
+
+for fname in *
+do
+  echo "In $fname..."
+  if [ -d "$fname" ]
+  then
+    # if fname is a directory, print dir name and cd into
+    echo "Converting files to wav format in file: $fname"
+    cd "$fname"
+  
+    # Format for loop from https://stackoverflow.com/questions/18621041/how-to-batch-convert-sph-files-to-wav-with-sox
+    for f in *.au 
+    do  
+      sox -t mp3 "$f" -b 16 -t wav "${f%.*}.wav"
+    done
+    cd ../ 
+  fi  
+done
+
+cd ../
