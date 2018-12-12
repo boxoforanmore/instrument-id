2198e6d871c24eddb659bb72117ecf1714382bb4
Peter Nielson <computergeekpete@gmail.com>
2018-12-12 11:20:28 -0700

    Added a script to classify individual samples

--- classify_instrument.py
+++ classify_instrument.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 import scipy
-import numpy
+import numpy as np
 import librosa
 from python_speech_features import mfcc
 from sys import argv
@@ -10,17 +10,18 @@ from pathlib import Path
 INSTRUMENTS = ['accordion', 'fiddle', 'flute', 'pennywhistle', 'uilleann']
 
 def generate_ceps(fname):
+    data = []
     X, sample_rate = librosa.core.load(fname)
 
     ceps = mfcc(X)
+    num_ceps = len(ceps)
 
-    return [np.mean(ceps[num_ceps//2:(num_ceps*9)//10], axis=0)]
+    return np.mean(ceps[num_ceps//2:(num_ceps*9)//10], axis=0)
 
 
 if '__main__' == __name__:
     length = len(argv)
     model = keras.models.load_model('model/instrument_id.h5')
-
 
     fnum = 1
     while (fnum < length):
@@ -28,10 +29,14 @@ if '__main__' == __name__:
         print('------------------------------------')
         print()
         print('Converting file: ', str(argv[fnum]))
-        ceps = generate_ceps(fnum)
-
+        ceps = generate_ceps(argv[fnum])
+        data = []
+        print(ceps.shape)
+        data.append(ceps)
+        ceps = np.array(data)
         print()
         print('Classifying sample with model')
         test_pred = model.predict_classes(ceps, verbose=0)
-        print('Guess: %s has an %s' % (fname, INSTRUMENTS[test_pred]))
+        print('Guess: %s has an %s' % (argv[fnum], INSTRUMENTS[test_pred[0]]))
         print()
+        fnum += 1
